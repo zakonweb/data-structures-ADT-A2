629128bb162232d980670ee8c5243633bb742252
Zafar Ali Khan <33575935+zakonweb@users.noreply.github.com>
2020-04-28 13:05:45 +0500

    Add files via upload

---
+++ QUEUES/Q - Simple LL/QUEUE as LL.vb
@@ -0,0 +1,71 @@
+﻿Module Module1
+    Structure QNode
+        Dim V As Char
+        Dim P As Integer
+    End Structure
+
+    Dim QUEUE(5) As QNode
+    Dim HP, TP, FP As Integer
+
+    Sub Main()
+        HP = 0
+        TP = 0
+        FP = 1
+
+        Dim menuOp As Integer
+        Dim d As Char
+
+        Do
+            Do
+                Console.WriteLine("1. Add data to QUEUE.")
+                Console.WriteLine("2. Remove data from QUEUE.")
+                Console.WriteLine("3. Initialise QUEUE.")
+                Console.WriteLine("4. Quit program.")
+                menuOp = Console.ReadLine
+            Loop Until menuOp >= 1 And menuOp <= 4
+
+            Select Case menuOp
+                Case 1
+                    Console.Write("Enter Value to Add :") : d = Console.ReadLine
+                    Call AddQ(d)
+                Case 2
+                    d = removeQ()
+                    Console.WriteLine("VALUE REMOVED WAS : " & d)
+                Case 3 : Call InitQ()
+            End Select
+        Loop Until menuOp = 4
+    End Sub
+
+    Sub AddQ(ByVal d As Char)
+        If FP = 0 Then
+            Console.WriteLine("OVERFLOW")
+        Else
+            HP = FP
+            QUEUE(HP).V = d
+            FP = QUEUE(HP).P
+        End If
+    End Sub
+
+    Sub InitQ()
+        Dim i As Integer = 0
+        For i = 1 To 4
+            QUEUE(i).V = ""
+            QUEUE(i).P = i + 1
+        Next
+
+        QUEUE(5).P = 0
+        HP = 0
+        TP = 0
+        FP = 1
+    End Sub
+
+    Function removeQ() As Char
+        If HP = TP Then
+            Console.WriteLine("UNDERFLOW")
+            Return "0"
+        Else
+            TP += 1
+            Return QUEUE(TP).V
+        End If
+    End Function
+End Module
---
+++ STACKS/Stack - Simple - Array 2/Stack2020.vb
@@ -0,0 +1,60 @@
+﻿Module Module1
+    Dim Stack(8) As Integer
+    Const FullStack = 8
+    Const BaseOfStackPointer = 0
+    Dim TopOfStackPointer = -1
+    Const NullPointer = -1
+
+    Sub Main()
+        Dim choice, Value As Integer
+        Do
+            Console.Clear()
+            Console.WriteLine("1. Push to stack.")
+            Console.WriteLine("2. Pop from stack.")
+            Console.WriteLine("3. Exit program.")
+            Do
+                Console.Write("Enter your choice 1..3 : ")
+                choice = Console.ReadLine
+            Loop Until choice >= 1 And choice <= 3
+
+            Select Case choice
+                Case 1 : Call Push()
+                Case 2
+                    Value = Pop()
+                    If Value = -1 Then
+                        Console.WriteLine("Underflow, Stack is empty. No value to pop.")
+                        Console.ReadKey()
+                    Else
+                        Console.WriteLine("Stack Value Poped : " & Value)
+                        Console.ReadKey()
+                    End If
+            End Select
+        Loop Until choice = 3
+
+    End Sub
+
+    Sub Push()
+        Dim Value As Integer
+        If TopOfStackPointer = FullStack Then
+            Console.WriteLine("Overflow, Stack is full. No more value can be added.")
+            Console.ReadKey()
+        Else
+            Console.Write("Enter value to add to stack: ")
+            Value = Console.ReadLine
+            TopOfStackPointer = TopOfStackPointer + 1
+            Stack(TopOfStackPointer) = Value
+        End If
+    End Sub
+
+    Function Pop() As Integer
+        Dim Value As Integer
+        If TopOfStackPointer = NullPointer Then
+            Value = NullPointer
+        Else
+            Value = Stack(TopOfStackPointer)
+            TopOfStackPointer = TopOfStackPointer - 1
+        End If
+
+        Return Value
+    End Function
+End Module
